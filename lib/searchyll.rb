9af6f136e346f2d389c8514ba169bb694a0e5374
Allison Zadrozny <allison@zadrozny.com>
2016-07-16 13:51:08 -0700

    Add print statement to indexer

--- lib/searchyll.rb
+++ lib/searchyll.rb
@@ -23,10 +23,13 @@ begin
     # strip html
     nokogiri_doc = Nokogiri::HTML(post.output)
 
+    puts %(indexing #{post.url})
+
     indexer = indexers[post.site]
     indexer << post.data.merge({
       id:     post.id,
-      text: nokogiri_doc.xpath("//article//text()").to_s.gsub(/\s+/, " ")
+      url:    post.url,
+      text:   nokogiri_doc.xpath("//article//text()").to_s.gsub(/\s+/, " ")
     })
   end
 
