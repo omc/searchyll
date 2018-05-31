0edbcec6424d7d8a14d05197933888fae5d10db6
Luca Lusso <lussoluca@gmail.com>
2018-05-31 08:24:21 +0200

    Use page.title instead of page.name

--- lib/searchyll.rb
+++ lib/searchyll.rb
@@ -28,7 +28,7 @@ begin
 
     indexer = indexers[page.site]
     indexer << page.data.merge({
-      id:     page.name,
+      id:     page.title,
       url:    page.url,
       text:   nokogiri_doc.xpath("//article//text()").to_s.gsub(/\s+/, " ")
     })
