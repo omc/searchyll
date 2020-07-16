0988fa1f4f0e91e3bb38de003153d7cf9ae743ce
Daniel Granerud <daniel.granerud@payex.com>
2020-07-16 13:27:06 +0200

    Added TestSite to test Configuration

--- spec/searchyll/configuration_spec.rb
+++ spec/searchyll/configuration_spec.rb
@@ -1,6 +1,13 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 require "jekyll"
 
+class TestSite
+  attr_accessor :config
+  def initialize(config)
+    @config = config
+  end
+end
+
 describe Searchyll::Configuration do
 
   it 'is expected to return true when production and flag not set' do
@@ -8,13 +15,6 @@ describe Searchyll::Configuration do
       'environment' => 'production',
       'elasticsearch' => {
         'production_only' => false
-      },
-      'source' => __dir__,
-      'destination' => __dir__,
-      'cache_dir' => __dir__,
-      'permalink' => __dir__,
-      'liquid' => {
-        'error_mode' => false
       }
     }
     site = TestSite.new site_config
@@ -27,13 +27,18 @@ describe Searchyll::Configuration do
       'environment' => 'production',
       'elasticsearch' => {
         'production_only' => true
-      },
-      'source' => __dir__,
-      'destination' => __dir__,
-      'cache_dir' => __dir__,
-      'permalink' => __dir__,
-      'liquid' => {
-        'error_mode' => false
+      }
+    }
+    site = TestSite.new site_config
+    conf = Searchyll::Configuration.new site
+    expect(conf.elasticsearch_production_only?).to eq(true)
+  end
+
+  it 'is expected to return false when not production and flag is not set' do
+    site_config = {
+      'environment' => 'not_production',
+      'elasticsearch' => {
+        'production_only' => false
       }
     }
     site = TestSite.new site_config
@@ -45,33 +50,7 @@ describe Searchyll::Configuration do
     site_config = {
       'environment' => 'not_production',
       'elasticsearch' => {
-        'production_only' => true
-      },
-      'source' => __dir__,
-      'destination' => __dir__,
-      'cache_dir' => __dir__,
-      'permalink' => __dir__,
-      'liquid' => {
-        'error_mode' => false
-      }
-    }
-    site = TestSite.new site_config
-    conf = Searchyll::Configuration.new site
-    expect(conf.elasticsearch_production_only?).to eq(false)
-  end
-
-  it 'is expected to return false when not production and flag is not set' do
-    site_config = {
-      'environment' => 'not_production',
-      'elasticsearch' => {
         'production_only' => false
-      },
-      'source' => __dir__,
-      'destination' => __dir__,
-      'cache_dir' => __dir__,
-      'permalink' => __dir__,
-      'liquid' => {
-        'error_mode' => false
       }
     }
     site = TestSite.new site_config
